6d9af356214aad80fdf1d800f63200dc8fa8df2c
Emma Laptop <emmanuelbeneboh@gmail.com>
2025-08-21 09:16:37 -0400

    navbar with cart and profile icons

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-gray-400'>
+        {/* Hero Left Side*/}
+        <div className=''>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
