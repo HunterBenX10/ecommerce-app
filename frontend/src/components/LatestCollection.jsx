4ae36709e98af29e1af0368c326311f962aa9557
Emma Laptop <emmanuelbeneboh@gmail.com>
2025-08-22 18:36:21 -0400

    idk

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Title from './Title'
+import { ShopContext } from '../context/ShopContext';
+import React from 'react';
+
 
 const LatestCollection = () => {
-  return (
-    <div>LatestCollection</div>
-  )
+
+    // const { products } = useContext(ShopContext);
+
+    return (
+      <div className='my-10'>
+          <div className="text-center py-8 text-3xl">
+              <Title text1="LATEST" text2="COLLECTIONS" />
+              <p className='w-w/4 m-auto text-xs sm:text-base text-gray-600'> Discover our latest arrivals and exclusive collections.
+              </p>
+          </div>
+      </div>
+    )
 }
 
 export default LatestCollection
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,14 +1,15 @@
 import { createContext } from "react";
 import { products } from "../assets/assets";    
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-    const currency = '$'
+    const currency = '$';
     const delivery_fee = 10;
     
     const value = {
-        products,
+        products,   
         currency,
         delivery_fee
     }
