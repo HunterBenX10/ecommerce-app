13bec964a67003744b02f40a42e6580c58bffe1f
Emma Laptop <emmanuelbeneboh@gmail.com>
2025-08-22 17:18:38 -0400

    menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -43,6 +43,8 @@ const Navbar = () => {
           <img src={assets.cart_icon} className='w-5 min-w-5' alt='cart icon'/>
           <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
         </Link>
+
+        <img src={assets.menu_icon} className='w-5 sm:hidden cursor-pointer' alt='menu icon'/>
       </div>
     </div>
 
