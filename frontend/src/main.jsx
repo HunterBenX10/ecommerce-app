f2b8afe1de0336354c64929ed9bcb8073148fa3e
Ben <emmanuelbeneboh@gmail.com>
2025-09-02 18:11:17 -0400

    fixed imports and added LatestCollection and ProductItem components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom' 
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { ShopContext } from './context/ShopContext.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import ShopContextProvider from "./context/ShopContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ShopContext.Provider>
+    <ShopContextProvider>
       <App />
-    </ShopContext.Provider>
+    </ShopContextProvider>
   </BrowserRouter>
-
-)
+);
