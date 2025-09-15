9776780590e4208a8ce48a7833c0d23483e0747f
Ben <emmanuelbeneboh@gmail.com>
2025-09-15 10:03:55 -0400

    fixed margins

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,22 +17,26 @@ import { Analytics } from "@vercel/analytics/react";
 
 const App = () => {
   return (
-    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
-      <ToastContainer />
-      <Navbar />
-      <SearchBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/collection" element={<Collection />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/product/:productId" element={<Product />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/place-order" element={<PlaceOrder />} />
-      </Routes>
-      <Footer />
+    <div className="min-h-screen flex flex-col">
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] flex-1 flex flex-col">
+        <ToastContainer />
+        <Navbar />
+        <SearchBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/place-order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+        <Footer />
+      </div>
     </div>
   );
 };
