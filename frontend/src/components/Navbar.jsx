6ecbe2927398d093f097e56bd79d6a22b71f8938
Ben <emmanuelbeneboh@gmail.com>
2025-09-15 09:56:52 -0400

    fixed cart icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -42,26 +42,30 @@ const Navbar = () => {
         />
 
         <div className="group relative">
-          <Link to='/login  '>
-          <img
-            className="w-5 cursor-pointer"
-            src={assets.profile_icon}
-            alt="profile icon"
-          />
+          <Link to="/login  ">
+            <img
+              className="w-5 cursor-pointer"
+              src={assets.profile_icon}
+              alt="profile icon"
+            />
           </Link>
           <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4 ">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">Profile</p>
-              <Link to="/orders" className="cursor-pointer hover:text-black">Orders</Link>
+              <Link to="/orders" className="cursor-pointer hover:text-black">
+                Orders
+              </Link>
               <p className="cursor-pointer hover:text-black">Logout</p>
             </div>
           </div>
         </div>
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="cart icon" />
-          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
-            {getCartCount()}
-          </p>
+          {getCartCount() > 0 && (
+            <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+              {getCartCount()}
+            </p>
+          )}
         </Link>
 
         <img
